72e847a4e68b4e22f2634ebc35ed4700ccb7a0d4
protossgenius <guyvejianglou@outlook.com>
2020-03-28 01:01:05 +0800

    fix the bug about vim don't show go's function def when call a function

--- programs/vim/config/install.sh
+++ programs/vim/config/install.sh
@@ -3,5 +3,5 @@ mkdir ~/.vim
 mkdir ~/.vim/bundle
 cd ~/.vim/bundle && git clone https://github.com/gmarik/Vundle.vim
 vim +PluginInstall 
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --all
+cd ~/.vim/bundle/YouCompleteMe && ./install.py --gocode-completer  --clang-completer
 vim +GoInstallBinaries 
