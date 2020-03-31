777cf3e5949032f2c660a6c0d9f6a0b892068a3a
Protoss Genius <guyvejianglou@outlook.com>
2020-03-31 10:54:52 +0000

    add rely about protoc-gen-go

--- responses/SureMoonNet/check.sh
+++ responses/SureMoonNet/check.sh
@@ -1,1 +1,2 @@
 cd $GOPATH/src/github.com/ProtossGenius/SureMoonNet
+smn_itf2rpc_go # check SureMoonNet's install
--- responses/SureMoonNet/install.sh
+++ responses/SureMoonNet/install.sh
@@ -1,2 +1,4 @@
 cd $GOPATH/src/github.com/ProtossGenius 
 git clone git@github.com:ProtossGenius/SureMoonNet.git
+make install
+go get -u github.com/golang/protobuf/protoc-gen-go
