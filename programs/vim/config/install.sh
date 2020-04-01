3bf528071c5d27725884e732e55700de9b9581f6
Protoss Genius <guyvejianglou@outlook.com>
2020-04-01 07:12:34 +0000

    vim config after vim install, vim should exit and continue next install.

--- programs/vim/config/install.sh
+++ programs/vim/config/install.sh
@@ -2,6 +2,6 @@ cp ~/.smcfg/programs/vim/config/.vimrc ~/
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 cd ~/.vim/bundle && git clone https://github.com/gmarik/Vundle.vim
-vim +PluginInstall
+vim +PluginInstall <~/.smcfg/programs/vim/config/vimexit 
 cd ~/.vim/bundle/YouCompleteMe && ./install.py --gocode-completer  --clang-completer
-vim +GoInstallBinaries 
+vim +GoInstallBinaries <~/.smcfg/programs/vim/config/vimexit
