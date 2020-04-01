79d5a9b606da0627ab525e0bb2cbf39d8e69a6f5
root <root@sh-suirenjie.SNDA.ROOT.CORP>
2020-04-01 14:58:31 +0800

    bug fix error in ubuntu

--- programs/vim/config/install.sh
+++ programs/vim/config/install.sh
@@ -2,6 +2,6 @@ mv ~/.smcfg/programs/vim/config/.vimrc ~/
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 cd ~/.vim/bundle && git clone https://github.com/gmarik/Vundle.vim
-vim +PluginInstall +q +q +q +q
+vim +PluginInstall
 cd ~/.vim/bundle/YouCompleteMe && ./install.py --gocode-completer  --clang-completer
-vim +GoInstallBinaries +q +q +q +q
+vim +GoInstallBinaries 
