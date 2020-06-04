c495e3629a55aa3c3a6053c3f77acd9f507f3927
s.m <guyejianglou@outlook.com>
2020-06-04 14:29:25 +0800

    "copy redis's conf"

--- libs/redis6/install.sh
+++ libs/redis6/install.sh
@@ -3,3 +3,5 @@ smcfg -update libs/redis6/download
 tar -zxvf /tmp/redis-6.0.4.tar.gz  
 cd redis-6.0.4
 make && make install
+mkdir /usr/local/redis
+cp /tmp/redis-6.0.4/redis.conf /usr/local/redis/redis.conf
