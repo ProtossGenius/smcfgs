633c80127c3c6d616106f1513b828ffa72c27846
s.m <guyejianglou@outlook.com>
2020-09-29 18:56:56 +0800

    "gcc10.2's compile. "

--- programs/gcc10.2/ubuntu.install.sh
+++ programs/gcc10.2/ubuntu.install.sh
@@ -1,4 +1,5 @@
 cd /tmp/
+apt-get install build-essential --fix-missing -y
 smwget 941a8674ea2eeb33f5c30ecf08124874 gcc-10.2.0.tar.gz http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-10.2.0/gcc-10.2.0.tar.gz
 #wget https://ftp.gnu.org/gnu/gcc/gcc-6.3.0/gcc-6.3.0.tar.bz2
 tar zxvf  gcc-10.2.0.tar.gz 
@@ -8,4 +9,5 @@ mkdir build
 cd build
 ../configure --enable-checking=release --enable-languages=c,c++ --disable-multilib
 make 
+apt remove gcc* -y
 make install
