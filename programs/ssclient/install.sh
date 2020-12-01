7de56824579073078ea93384a4d920da131dc1e9
suremoon <protossgenius@gmail.com>
2020-12-01 23:19:25 +0800

    "update ssclient"

--- programs/ssclient/install.sh
+++ programs/ssclient/install.sh
@@ -1,5 +1,5 @@
 pip3 install shadowsocks
-cp cfg.json /etc/shadowsocks.cfg.json
+cp clt.json /etc/shadowsocks.cfg.json
 ssclient -c /etc/shadowsocks.cfg.json -d start
 go get -u github.com/ProtossGenius/kcptun/cmd/kcptun-client 
 
