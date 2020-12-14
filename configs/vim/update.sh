4761f472bb4cd9b239c9e8e6fe05dfd2103ebf26
suremoon <guyvejianglou@outlook.com>
2020-12-14 16:02:16 +0800

    "copy dir shoud with -r"

--- configs/vim/update.sh
+++ configs/vim/update.sh
@@ -4,8 +4,8 @@ ctags --fields=+niazS --extra=+q --c++-kinds=+px -R `pwd`
 vim_cfg=~/.smcfg/configs/vim/
 cp $vim_cfg/.vimrc ~/.vimrc
 cp $vim_cfg/.ctags ~/.ctags
-cp $vim_cfg/UltiSnips ~/.vim/UltiSnips
-vim +PlugUpdate  < $vim_cfg/vimexit 
+cp $vim_cfg/UltiSnips ~/.vim/UltiSnips -r
+vim +PlugUpdate  < $vim_cfg/vimexit
 cp $vim_cfg/.ycm_extra_conf.py ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
 cd ~/.vim/plugged/YouCompleteMe 
 git submodule update --init --recursive 
