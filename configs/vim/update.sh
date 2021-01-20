14770cffe3638c355b4b26481574ca7be6e4a152
suremoon <guyvejianglou@outlook.com>
2021-01-20 10:38:32 +0800

    "add git pull for vim's YCM update."

--- configs/vim/update.sh
+++ configs/vim/update.sh
@@ -8,6 +8,7 @@ cp $vim_cfg/UltiSnips ~/.vim -r
 vim +PlugUpdate  < $vim_cfg/vimexit
 cp $vim_cfg/.ycm_extra_conf.py ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
 cd ~/.vim/plugged/YouCompleteMe 
+git pull
 git submodule update --init --recursive 
 ./install.py --gocode-completer  --clang-completer
 vim +GoUpdateBinaries  < $vim_cfg/vimexit 
