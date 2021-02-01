befb05029f4e22b3349c79812d13751a9e81963a
protossgenius <guyvejianglou@outlook.com>
2021-02-02 00:00:53 +0800

    "add a example use shell."

--- programs/ssclient/install.sh
+++ programs/ssclient/install.sh
@@ -1,5 +1,6 @@
 pip3 install shadowsocks
 sudo cp clt.json /etc/shadowsocks.cfg.json
+cp ~/.smcfg/programs/ssclient/fq.sh ~/fq.sh
 ssclient -c /etc/shadowsocks.cfg.json -d start
 go get -u github.com/ProtossGenius/kcptun/cmd/kcptun-client 
 
