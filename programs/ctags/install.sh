8c8abe10684db5d59b668a30acd1dfff1f377a07
suremoon <ProtossGenius@gmail.com>
2021-04-16 21:28:26 +0800

    "update response, and let ctags's install to /usr/local/bin"

--- programs/ctags/install.sh
+++ programs/ctags/install.sh
@@ -2,6 +2,6 @@ cd /tmp
 git clone https://github.com/universal-ctags/ctags.git
 cd ctags
 ./autogen.sh
-./configure --prefix=/usr # defaults to /usr/local
+./configure  # defaults to /usr/local
 make -j8
 sudo make install # may require extra privileges depending on where to install
