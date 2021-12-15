c7ccced414bedf45b1c4d8916500d2733668e026
suremoon <ProtossGenius@gmail.com>
2021-12-15 22:16:01 +0800

    "update."

--- configs/vim/update.sh
+++ configs/vim/update.sh
@@ -4,9 +4,9 @@ cp $vim_cfg/.ctags ~/.ctags
 cp $vim_cfg/UltiSnips ~/.vim -r
 cp $vim_cfg/.clang-format ~/
 vim +PlugUpdate  < $vim_cfg/vimexit
-cp $vim_cfg/.ycm_extra_conf.py ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
 cd ~/.vim/plugged/YouCompleteMe 
 git pull
 git submodule update --init --recursive 
 ./install.py --gocode-completer  --clang-completer
+cp $vim_cfg/.ycm_extra_conf.py ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
 vim +GoUpdateBinaries  < $vim_cfg/vimexit 
