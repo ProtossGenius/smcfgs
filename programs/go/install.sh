01e191d009341a9ff8d7cfa532cf17270422dd25
suremoon <protossgeniue@gmail.com>
2024-05-27 21:11:57 +0800

    go install

--- programs/go/install.sh
+++ programs/go/install.sh
@@ -1,6 +1,5 @@
 cd /tmp/
-
-https://golang.google.cn/dl/go1.22.3.linux-amd64.tar.gz 
+wget https://golang.google.cn/dl/go1.22.3.linux-amd64.tar.gz 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin 
 cp ~/.smcfg/programs/go/go.sh /tmp/go.sh
